78eae11c359b05e1c99df6a3aba8b425e185e77f
adeliashahahahaha <asha.syrn5@gmail.com>
2024-12-15 11:52:36 +0700

    update kompen selesai v1 - ADM

--- resources/views/aUpdateKompen/index.blade.php
+++ resources/views/aUpdateKompen/index.blade.php
@@ -16,18 +16,52 @@
         <table class="table table-bordered table-striped table-hover table-sm">
             <thead>
                 <tr>
-                    <th>No</th>
-                    <th>Nama Tugas</th>
-                    <th>Nama Mahasiswa</th>
-                    <th>Pemberi Tugas</th>
-                    <th>Progres</th>
-                    <th>Berita Acara</th>
-                    <th>Aksi</th>
+                    <th class="text-center">No</th>
+                    <th class="text-center">Nama Tugas</th>
+                    <th class="text-center">Nama Mahasiswa</th>
+                    <th class="text-center">Pemberi Tugas</th>
+                    <th class="text-center">Progres</th>
+                    <th class="text-center">Berita Acara</th>
+                    <th class="text-center">Aksi</th>
                 </tr>
             </thead>
+            <tbody>
+                @foreach ($data as $item)
+                <tr>
+                    <td class="text-center">{{ $item['no'] }}</td>
+                    <td class="text-center">{{ $item['nama_tugas'] }}</td>
+                    <td class="text-center">{{ $item['nama_mahasiswa'] }}</td>
+                    <td class="text-center">{{ $item['pemberi_tugas'] }}</td>
+                    <td class="text-center">{{ $item['progres'] }}</td>
+
+                    <td class="text-center">
+                        @if ($item['berita_acara'])
+                            <a href="{{ $item['berita_acara'] }}" target="_blank">
+                                <i class="fas fa-file text-primary fa-2x"></i>
+                            </a>
+                        @else
+                            <span>-</span>
+                        @endif
+                    </td>
+                    <td class="text-center">
+                        <form action="{{ route('aUpdateKompen.TugasSelesai', $item['id_progres_tugas']) }}" method="POST" style="display:inline;">
+                            @csrf
+                            <button type="submit" class="btn btn-success btn-sm">Tugas Selesai</button>
+                        </form>
+
+                        <form action="{{ route('aUpdateKompen.KompenDiterima', $item['id_riwayat']) }}" method="POST" style="display:inline;">
+                            @csrf
+                            <button type="submit" class="btn btn-primary btn-sm">Kompen Diterima</button>
+                        </form>
+                    </td>
+
+                </tr>
+                @endforeach
+            </tbody>
+
         </table>
     </div>
 </div>
-<div id="myModal" class="modal fade animate shake" tabindex="-1" role="dialog" data backdrop="static"
+<div id="myModal" class="modal fade animate shake" tabindex="-1" role="dialog" data-backdrop="static"
     data-keyboard="false" data-width="75%" aria-hidden="true"></div>
 @endsection

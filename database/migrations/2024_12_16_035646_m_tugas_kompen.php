45aa5a9c1c258423e7af09da93b332f76024c602
nadhif1867 <0106.nadhif@gmail.com>
2024-12-16 18:21:40 +0700

    tugas kompen migration

--- database/migrations/2024_12_16_035646_m_tugas_kompen.php
+++ database/migrations/2024_12_16_035646_m_tugas_kompen.php
@@ -14,18 +14,18 @@ return new class extends Migration
         Schema::create('m_tugas_kompen', function (Blueprint $table) {
             $table->id('id_tugas_kompen');
             $table->unsignedBigInteger('id_mahasiswa')->index();
-            $table->unsignedBigInteger('id_tugas_admin')->index();
-            $table->unsignedBigInteger('id_tugas_dosen')->index();
-            $table->unsignedBigInteger('id_tugas_tendik')->index();
+            $table->unsignedBigInteger('id_tugas_admin')->nullable()->index();
+            $table->unsignedBigInteger('id_tugas_dosen')->nullable()->index();
+            $table->unsignedBigInteger('id_tugas_tendik')->nullable()->index();
             $table->enum('status_penerimaan', array('request', 'diterima', 'dibuka'));
             $table->dateTime('tanggal_apply');
             $table->integer('kuota');
             $table->timestamps();
 
             $table->foreign('id_mahasiswa')->references('id_mahasiswa')->on('m_mahasiswa');
-            $table->foreign('id_tugas_admin')->references('id_tugas_admin')->on('tugas_admin');
-            $table->foreign('id_tugas_dosen')->references('id_tugas_dosen')->on('tugas_dosen');
-            $table->foreign('id_tugas_tendik')->references('id_tugas_tendik')->on('tugas_tendik');
+            $table->foreign('id_tugas_admin')->references('id_tugas_admin')->on('tugas_admin')->onDelete('set null');
+            $table->foreign('id_tugas_dosen')->references('id_tugas_dosen')->on('tugas_dosen')->onDelete('set null');
+            $table->foreign('id_tugas_tendik')->references('id_tugas_tendik')->on('tugas_tendik')->onDelete('set null');
         });
     }
 
